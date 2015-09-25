3a4f0b8dd3866f0aa2382eafa20047e7a17ff959
Sung Chi <sungchi91@gmail.com>
2015-09-25 00:31:33 -0700

    added user registration fields

--- models.py
+++ models.py
@@ -10,7 +10,7 @@ class User(auth_models.User):
   zip_code = ndb.IntegerProperty(default = 0)
   credit_card = ndb.IntegerProperty(default = 0)
   security_code = ndb.IntegerProperty(default = 0)
-  expiration_date = ndb.DateTimeProperty()
+  expiration_date = ndb.DateProperty()
   # Foodie/Expert
   account_type = ndb.StringProperty()
 
