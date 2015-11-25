ba7b6fd6ce61a2b9e2f745c51bc2e78c0ad8c053
jmelberg <jordan.melberg@gmail.com>
2015-11-24 17:06:36 -0800

    fixed merge finally

--- payments.py
+++ payments.py
@@ -1,32 +1,5 @@
 import requests
 import json
-<<<<<<< HEAD
-# from models import PaymentLinks, PaymentModel
-
-# class CreatePaymentExample:
-#     def __init__(self):
-#         test = requests.get('http://107.170.240.130:3000/createpayment')
-#         print test
-
-# class CreatePayment:
-#     def __init__(self, foodie, expert, welink, price, accountID, short_desc):
-#         create = requests.post('http://107.170.240.130:3000/createpayment', data={"welink": welink, "price":price, "accountID":accountID, "short_desc":short_desc})
-#         link = create.json()
-#         print link["preapproval_uri"]
-#         payment = PaymentModel()
-#         payment.confirm = False
-#         payment.link = link["preapproval_uri"]
-#         payment.foodie = foodie
-#         payment.expert = expert
-#         payment.amount = price
-#         payment.put()
-
-# class ChargePayment:
-#     def __init__(self, future_id, price, accountID, desc):
-#         charge = requests.post('http://107.170.240.130:3000/chargepayment', data={"future_id":future_id, "price":price, "account_id":accountID, "desc":desc})
-#         print charge.json()
-=======
->>>>>>> update
 
 def AddCreditCard(name, email, creditcard, expmonth, expyear, address, city, state, cvv, zipcode):
     add = requests.post('http://107.170.240.130:3000/addcreditcard', data={"user_name": name, "email":email, "cc_number":creditcard, "cvv":cvv, "expiration_month":expmonth, "expiration_year":expyear, "address1":address, "city":city, "state":state, "country":"US", "zip":zipcode})
